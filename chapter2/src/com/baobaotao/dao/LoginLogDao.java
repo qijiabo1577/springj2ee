caf2f3fbba388cc333ae40fdca3a08a3027b8e0d
qijiabo <qijiabo@hisign.com.cn>
2014-10-21 10:31:31 +0800

    edit

--- chapter2/src/com/baobaotao/dao/LoginLogDao.java
+++ chapter2/src/com/baobaotao/dao/LoginLogDao.java
@@ -1,15 +1,11 @@
 package com.baobaotao.dao;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowCallbackHandler;
 import org.springframework.stereotype.Repository;
 
 import com.baobaotao.domain.LoginLog;
-import com.baobaotao.domain.User;
 
 @Repository  //通过spring注解定义一个DAO
 public class LoginLogDao {
