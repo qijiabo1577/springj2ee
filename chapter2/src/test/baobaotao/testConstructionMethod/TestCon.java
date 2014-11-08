9ead52737c97406fa58ebc9cf9c1f5b59f014aae
Administrator <Administrator@PC201402181321>
2014-11-08 19:32:01 +0800

    beanFactroy生命周期测试 修改java工程编码格式-GBK

---
+++ chapter2/src/test/baobaotao/testConstructionMethod/TestCon.java
@@ -0,0 +1,28 @@
+package test.baobaotao.testConstructionMethod;
+
+public class TestCon {
+	
+	private String a1;
+	private String a2;
+	
+	public TestCon(String a1,String a2){
+		this.a1=a1;
+		this.a2=a2;
+	}
+	
+	public String getA1() {
+		return a1;
+	}
+
+	public void setA1(String a1) {
+		this.a1 = a1;
+	}
+
+	public String getA2() {
+		return a2;
+	}
+
+	public void setA2(String a2) {
+		this.a2 = a2;
+	}
+}
